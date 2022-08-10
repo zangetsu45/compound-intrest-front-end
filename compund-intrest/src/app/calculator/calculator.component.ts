298c542bb832743684523e3a7f758bf19ab6cdda
zangetsu45 <mayankgobind02875@gmail.com>
2022-08-11 01:05:56 +0530

    frontend done

--- compund-intrest/src/app/calculator/calculator.component.ts
+++ compund-intrest/src/app/calculator/calculator.component.ts
@@ -14,13 +14,81 @@ export class CalculatorComponent implements OnInit {
   principal_input = null;
   rate_input  = null;
   time_input = null;
-  result=10;
+  result=null;
    calculate() {
-   // var P=document.getElementById("principal")?.innerHTML!;
-    //var R=document.getElementById("rate")?.innerHTML!;
-    //var T=document.getElementById("time")?.innerHTML!;
-    //if(isNaN(P) && isNaN(R) && isNaN(T))
-    alert("error");}
+    var str = "Please enter ";
+    var flag = 0;
+    if(this.principal_input==null)
+    {
+      str+="Principal ";
+      flag=1;
+    }
+    if(this.rate_input==null)
+    {
+      str+="rate ";
+      flag=1;
+    }
+    if(this.time_input==null)
+    {
+      str+="time ";
+      flag=1;
+    }
+    if(flag=1){
+      alert(str);
+      return;
+    }
+   var xhr = new XMLHttpRequest();
+    var url = "http://localhost:8080/calculator/calculate";
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var res = xhr.responseText;
+            if(document.getElementById( "result" )){
+            document.getElementById( "result" )!.innerHTML=res;
+          }          
+            console.log(res);
+
+        }
+    }
+    var data =JSON.stringify({
+      "principal" : this.principal_input,
+      "rate" : this.rate_input,
+      "time" : this.time_input
+  });
+xhr.send(data);
+/*const dataToSend = JSON.stringify({
+  "principal" : 1000,
+  "rate" : 0.05,
+  "time" : 3
+});
+let dataReceived = ""; 
+fetch("http://localhost:8080/calculator/calculate", {//lvl/4200/lcl8080
+     credentials: "same-origin",
+     mode: "same-origin",
+     method: "get",
+     headers: { "Content-Type": "application/json" },
+     body: dataToSend
+})
+
+    .then(resp => {
+        if (resp.status === 200) {
+            return resp.json()
+        } else {
+            console.log("Status: " + resp.status)
+            return Promise.reject("server")
+        }
+    })
+    .then(dataJson => {
+        dataReceived = JSON.parse(dataJson)
+    })
+    .catch(err => {
+        if (err === "server") return
+        console.log(err)
+    })
+
+  console.log(`Received: ${dataReceived}`)*/  }
+
   constructor() { }
 
   ngOnInit(): void {
