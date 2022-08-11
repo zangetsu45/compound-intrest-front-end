4fd34de48851e170b305bc3e73eecf57330b08b6
zangetsu45 <mayankgobind02875@gmail.com>
2022-08-11 11:45:27 +0530

    done

--- compund-intrest/src/app/calculator/calculator.component.ts
+++ compund-intrest/src/app/calculator/calculator.component.ts
@@ -57,12 +57,8 @@ export class CalculatorComponent implements OnInit {
       "time" : this.time_input
   });
 xhr.send(data);
-<<<<<<< HEAD
-  }
-=======
- }
->>>>>>> c114d3bb24c4e52056f1fb994bac9b11497fdcd1
 
+ }
   constructor() { }
 
   ngOnInit(): void {
